7d3905acb85fa16c89a9dc21e1da1a99ede8eb96
cflep <cflep219@hotmail.com>
2022-10-26 17:43:31 +0200

    Changed imports

--- RootToNN/__init__.py
+++ RootToNN/__init__.py
@@ -1,2 +1,4 @@
-import Simulation
-import Tensor3d
+from RootToNN.Simulation import Simulation
+from RootToNN.Tensor3d import Tensor3d
+from RootToNN.Event import Event
+from RootToNN.SiFiCC_Module import SiFiCC_Module
