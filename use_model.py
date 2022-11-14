93c8bfbb3e8013722fd9a0988950a30c1617bb33
cflep <cflep219@hotmail.com>
2022-11-14 14:47:06 +0100

    imported custom loss

--- use_model.py
+++ use_model.py
@@ -4,9 +4,10 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 
 path = "1.npz"
+index = 45
 
 with np.load(path) as data:
-    input_data = data["all_events_input"]
+    input_data  = data["all_events_input"]
     output_data = data["all_events_output"]
 
     print(input_data.shape)
@@ -24,21 +25,32 @@ with np.load(path) as data:
     X_test  = input_data[valset_index:]
     Y_test  = output_data[valset_index:]
 
-    model = keras.models.load_model('firstNN_model1')
-    f_X_test = model.predict(X_test)
-    print("Xvalue=%s, Difference=%s" % (X_test[300], abs(f_X_test[300] - Y_test[300])))
+    model       = keras.models.load_model('firstNN_model2.h5', custom_objects={ 'loss': custom_fn(penalty_weight) })
+    model = load_model(modelFile)
+    f_X_test    = model.predict(X_test)
+    print("Xvalue=%s, Difference=%s" % (X_test[index], abs(f_X_test[index] - Y_test[index])))
     E_err = []
     p_err = []
-    for i in range(len(f_X_test[300] - Y_test[300])):
+    p_true = []
+    p_reco = []
+    for i in range(len(f_X_test[index] - Y_test[index])):
         Ex = []
         px = []
-        for j in range(len(f_X_test[300][i])):
-            Ex.append(abs(f_X_test[300][i][j][0])) # - Y_test[300][i][j][0]))
-            px.append(abs(f_X_test[300][i][j][1])) # - Y_test[300][i][j][1]))
+        ptx = []
+        prx = []
+        for j in range(len(f_X_test[index][i])):
+            Ex.append(abs(f_X_test[index][i][j][0] - Y_test[index][i][j][0]))
+            px.append(abs(f_X_test[index][i][j][1] - Y_test[index][i][j][1]))
+            ptx.append(Y_test[index][i][j][1])
+            prx.append(f_X_test[index][i][j][1])
         E_err.append(np.array(Ex))
         p_err.append(np.array(px))
+        p_true.append(ptx)
+        p_reco.append(prx)
     E_err = np.array(E_err)
     p_err = np.array(p_err)
+    p_true = np.array(p_true)
+    p_reco = np.array(p_reco)
             
     print(E_err)
     print(50*'-')
@@ -47,5 +59,9 @@ with np.load(path) as data:
     plt.matshow(E_err)
     plt.show()
     plt.matshow(p_err)
+    plt.show()
+    plt.matshow(p_true)
+    plt.show()
+    plt.matshow(p_reco)
     plt.show()
 
